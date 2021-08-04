b3a084ba8379c5c96f0924f839faa57a807a78cb
caturpep <caturputra.ep@gmail.com>
2021-08-04 10:02:22 +0700

    cutscene

---
+++ Assets/Scripts/cutScenes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cutScenes : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject cutScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cutScene.SetActive(true);
+        player.SetActive(false);
+        StartCoroutine(FinishCut());
+    }
+
+    IEnumerator FinishCut()
+    {
+        yield return new WaitForSeconds(5);
+        player.SetActive(true);
+        cutScene.SetActive(false);
+    }
+}
