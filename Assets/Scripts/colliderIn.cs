6906016e8e262aaefeb17185881f861e84b326da
dimasr11 <dimasramadhan081@gmail.com>
2021-08-13 17:02:51 +0700

    booth tata

---
+++ Assets/Scripts/colliderIn.cs
@@ -0,0 +1,13 @@
+﻿        using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class colliderIn : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Terdeteksi");
+        SceneManager.LoadScene("ExpoUM");
+    }
+}
